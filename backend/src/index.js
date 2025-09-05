64aaca7ac7418608d0c238a9fbf2a7ae67ed8af0
Kuldeep <kunalrajput67718@gmail.com>
2025-09-05 21:57:27 +0530

    Add backend structure with models for Application, Company, Job, and User; update scripts and entry point

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import connectDB from "./db/connect.db.js";
+import { app } from "./app.js";
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(
+    app.listen(process.env.PORT, () => {
+      console.log(`SERVER IS RUNNING AT PORT::${process.env.PORT}`);
+    })
+  )
+  .catch((err) => console.error("MONGODB connection faild", err));
---
+++ backend/src/models/job.model.js
@@ -0,0 +1,50 @@
+import { Schema, model } from "mongoose";
+
+const jobSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  requirements: [
+    {
+      type: String,
+    },
+  ],
+  salary: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  jobType: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: Number,
+    required: true,
+  },
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: "Company",
+  },
+  created_by: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  applications: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Application",
+    },
+  ],
+},{timestamps:true});
+
+export const Job = model("Jon", jobSchema);
